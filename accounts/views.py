a83f1261203d8ba8a42464ab16006cc5d9063b32
Tuan Nguyen <python.study.0708@gmail.com>
2018-10-02 16:45:28 +0700

    added all authen for login, logout, signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def signup(request):
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objexts.get(username=request.POST['username'])
+                user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'Username is already exit.'})
+                print('Confirm password does not match.')
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=[password1])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
-            else:
-                return render(request, 'accounts/signup.html', {'error': 'Confirm password dose not match.'})
+        else:
+            return render(request, 'accounts/signup.html')
+            print('Confirm password does not match.')
     else:
-        return render(request, 'accounts/signup.html', {'error': 'Please input the information.'})
+        return render(request, 'accounts/signup.html', {'error': 'Confirm password does not match.'})
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password1'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
-    # TODO Need to rout to homepage
-    # and dont forget to logout
-    return render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        auth.logout(request)
+        return redirect('home')
