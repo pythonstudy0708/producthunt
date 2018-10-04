030d511396d8984ce75a3d7eef191dd98dadc717
Tuan Nguyen <python.study.0708@gmail.com>
2018-10-04 11:06:24 +0700

    add create products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Product class
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+    url = models.TextField()
+    votes_total = models.IntegerField(default = 1)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
+
+#hunter
--- products/views.py
+++ products/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['image'] and request.POST['icon']:
+            product = Product()
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            product.image = request.POST['image']
+            product.icon = request.POST['icon']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save
+            return redirect('home')
+
+        else:
+            return render(request, 'products/home.html', {'error':'All fields need to be provided'})
+    else:
+        return render(request, 'products/home.html')
